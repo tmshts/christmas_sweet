56720d97a8d1c364ffa621fc36e39687bf738ff4
tmshts <tomhatas@gmail.com>
2023-12-11 19:32:28 -0600

    update navbar

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -8,7 +8,6 @@ import foto from '../../assets/arbol.png'
 const Nav = styled.nav`
     width: 100%;
     height: 55px;
-    border-bottom: 2px solid #f1f1f1;
     display: flex;
     justify-content: space-between;
     background-color: #00A693;
